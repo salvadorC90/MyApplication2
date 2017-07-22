4719f83610046f8896fd58cadb219f2104b5eaf0
Salvador <bulazi665@gmail.com>
2017-07-22 16:22:29 -0700

    redbull in my veins

--- app/src/main/java/com/example/android/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -1,38 +1,72 @@
 package com.example.android.mymusicapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import static com.paypal.android.sdk.onetouch.core.metadata.ah.I;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView library = (TextView) findViewById(R.id.library);
+        library.setOnClickListener(this);
+
+        TextView browse = (TextView) findViewById(R.id.browse);
+        browse.setOnClickListener(this);
+
+        TextView radio = (TextView) findViewById(R.id.radio);
+        radio.setOnClickListener(this);
+
+        TextView store = (TextView) findViewById(R.id.store);
+        store.setOnClickListener(this);
+    }
+
     public void onClick(View v) {
 
-       switch(v.getId()) {
-           case R.id.library:
-               Intent libraryIntent = new Intent(MainActivity.this, LibraryActivity.class);
-               startActivity(libraryIntent);
-               break;
+        Class navigation;
 
-           case R.id.browse:
-               Intent browseIntent = new Intent(MainActivity.this, BrowseActivity.class);
-               startActivity(browseIntent);
-               break;
+        if (v.getId() == R.id.library) {
+            navigation = LibraryActivity.class;
+        } else if (v.getId() == R.id.browse) {
+            navigation = BrowseActivity.class;
+        } else if (v.getId() == R.id.radio) {
+            navigation = RadioActivity.class;
+        } else if (v.getId() == R.id.store) {
+            navigation = PaymentsActivity.class;
+        } else {
+            return;
+        }
 
-           case R.id.radio:
-               Intent radioIntent = new Intent(MainActivity.this, RadioActivity.class);
-               startActivity(radioIntent);
-               break;
+        Intent newActivity = new Intent(this, navigation);
+        startActivity(newActivity);
 
-           case R.id.store:
-               Intent storeIntent = new Intent(MainActivity.this, PaymentsActivity.class);
-               startActivity(storeIntent);
-               break;
+        switch (v.getId()) {
 
+            case R.id.library:
+                navigation = LibraryActivity.class;
+                break;
 
-       }
+            case R.id.browse:
+                navigation = BrowseActivity.class;
+                break;
+
+            case R.id.radio:
+                navigation = RadioActivity.class;
+                break;
+
+            case R.id.store:
+                navigation = PaymentsActivity.class;
+                break;
+
+        }
 
     }
 }
