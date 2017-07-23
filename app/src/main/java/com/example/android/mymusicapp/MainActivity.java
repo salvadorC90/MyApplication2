baf966456e38dbbaba011d0075806839304006ba
Salvador <bulazi665@gmail.com>
2017-07-23 14:50:59 -0700

    Game of Thrones

--- app/src/main/java/com/example/android/mymusicapp/MainActivity.java
+++ app/src/main/java/com/example/android/mymusicapp/MainActivity.java
@@ -9,55 +9,12 @@ import android.widget.TextView;
 import static com.paypal.android.sdk.onetouch.core.metadata.ah.I;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Nav {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView library = (TextView) findViewById(R.id.library);
-        library.setOnClickListener(this);
-
-        TextView browse = (TextView) findViewById(R.id.browse);
-        browse.setOnClickListener(this);
-
-        TextView radio = (TextView) findViewById(R.id.radio);
-        radio.setOnClickListener(this);
-
-        TextView store = (TextView) findViewById(R.id.store);
-        store.setOnClickListener(this);
     }
-
-    public void onClick(View v) {
-
-        Class navigation;
-
-        switch (v.getId()) {
-
-            case R.id.library:
-                navigation = LibraryActivity.class;
-                break;
-
-            case R.id.browse:
-                navigation = BrowseActivity.class;
-                break;
-
-            case R.id.radio:
-                navigation = RadioActivity.class;
-                break;
-
-            case R.id.store:
-                navigation = PaymentsActivity.class;
-                break;
-
-            default:
-                return;
-        }
-
-        Intent newActivity = new Intent(this, navigation);
-        startActivity(newActivity);
-
-    }
-
 }
---
+++ app/src/main/java/com/example/android/mymusicapp/Nav.java
@@ -0,0 +1,59 @@
+package com.example.android.mymusicapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import static com.paypal.android.sdk.onetouch.core.metadata.ah.I;
+
+
+public class Nav extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    public void onClick(View v) {
+
+        Class navigation;
+
+        TextView library = (TextView) findViewById(R.id.library);
+        library.setOnClickListener(this);
+
+        TextView browse = (TextView) findViewById(R.id.browse);
+        browse.setOnClickListener(this);
+
+        TextView radio = (TextView) findViewById(R.id.radio);
+        radio.setOnClickListener(this);
+
+        TextView store = (TextView) findViewById(R.id.store);
+        store.setOnClickListener(this);
+
+        switch (v.getId()) {
+
+            case R.id.library:
+                navigation = LibraryActivity.class;
+                break;
+
+            case R.id.browse:
+                navigation = BrowseActivity.class;
+                break;
+
+            case R.id.radio:
+                navigation = RadioActivity.class;
+                break;
+
+            case R.id.store:
+                navigation = PaymentsActivity.class;
+                break;
+
+            default:
+                return;
+        }
+
+        Intent newActivity = new Intent(this, navigation);
+        startActivity(newActivity);
+
+    }
+
+}
